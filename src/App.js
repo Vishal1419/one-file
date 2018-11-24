3a2c5d8b9d7387ed887aec1a89cc3027ae0e862a
Vishal Sherathiya <vishalsherathiya@gmail.com>
2018-11-25 01:09:29 +0530

    show electron app version in titlebar and move dependencies to dev-dependencies

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import { Provider } from 'react-redux';
+import isElectron from 'is-electron';
 
 import store from './store';
 import './assets/styles/styles.scss';
 import Routes from './Routes';
 
+let appVersion;
+if (isElectron()) {
+	appVersion = window.require('electron').remote.app.getVersion();	
+}
+
 const App = () => (
   <div className="App">
+    <Helmet>
+      <title>{`one-file-${appVersion}`}</title>
+    </Helmet>
     <Provider store={store}>
       <Routes />
     </Provider>
